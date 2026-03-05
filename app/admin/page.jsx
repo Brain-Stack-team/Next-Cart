24f423cf3e83bfda10b3c340c0e9fa1b3620ca47
md abu hanif mia <mohammadbitullah@gmail.com>
2026-03-05 13:05:49 +0600

    admin page update

---
+++ app/admin/page.jsx
@@ -0,0 +1,10 @@
+import AdminClient from "@/components/admin-client"
+
+export const metadata = {
+  title: "Admin Panel | Next-Cart",
+  description: "Next-Cart Admin Dashboard",
+}
+
+export default function AdminPage() {
+  return <AdminClient />
+}
